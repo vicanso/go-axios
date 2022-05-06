9b949e2e888c61d405bffa4c3953bd4d95da81f0
vicanso <vicansocanbico@gmail.com>
2022-05-06 20:20:23 +0800

    refactor: support read all function for less memory

--- adapter.go
+++ adapter.go
@@ -15,14 +15,41 @@
 package axios
 
 import (
-	"io/ioutil"
+	"io"
 	"net/http"
+	"strconv"
 )
 
 type (
 	// Adapter adapter function
 	Adapter func(config *Config) (resp *Response, err error)
 )
+
+// copy from io.ReadAll
+// ReadAll reads from r until an error or EOF and returns the data it read.
+// A successful call returns err == nil, not err == EOF. Because ReadAll is
+// defined to read from src until EOF, it does not treat an EOF from Read
+// as an error to be reported.
+func ReadAllInitCap(r io.Reader, initCap int) ([]byte, error) {
+	if initCap <= 0 {
+		initCap = 512
+	}
+	b := make([]byte, 0, initCap)
+	for {
+		if len(b) == cap(b) {
+			// Add more capacity (let append pick how much).
+			b = append(b, 0)[:len(b)]
+		}
+		n, err := r.Read(b[len(b):cap(b)])
+		b = b[:len(b)+n]
+		if err != nil {
+			if err == io.EOF {
+				err = nil
+			}
+			return b, err
+		}
+	}
+}
 
 func defaultAdapter(config *Config) (resp *Response, err error) {
 
@@ -45,7 +72,12 @@ func defaultAdapter(config *Config) (resp *Response, err error) {
 	}
 	// 读取数据
 	defer res.Body.Close()
-	data, err := ioutil.ReadAll(res.Body)
+	size := 0
+	contentLength := res.Header.Get("Content-Length")
+	if contentLength != "" {
+		size, _ = strconv.Atoi(contentLength)
+	}
+	data, err := ReadAllInitCap(res.Body, size)
 	if err != nil {
 		return
 	}
